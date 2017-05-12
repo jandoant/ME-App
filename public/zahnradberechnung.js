0cd9a891fef7a02f200026318c7f36e2287eb511
jandoant <jandoant@outlook.de>
2017-05-12 11:03:01 +0200

    Übersetzung und Abweichung von der Soll-Übersetzung implementieren

--- public/zahnradberechnung.js
+++ public/zahnradberechnung.js
@@ -1,38 +1,55 @@
 console.log('Willkommen bei der Zahnradberechnung');
 
-let btn_counter = 0;
-let m_n = 0;
-let z1 = 0;
-let z2 = 0;
-let beta = 0;
-let alpha_n = 20;
-
 //DOM-Objekte mit Javascript verknüpfen
 var input_m_n = document.getElementById('js-input-modul');
 var input_alpha = document.getElementById('js-input-alpha');
 var input_beta = document.getElementById('js-input-beta');
 var input_z1 = document.getElementById('js-input-z1');
 var input_z2 = document.getElementById('js-input-z2');
+var input_i = document.getElementById('js-show-uebersetzung');
+var input_i_soll = document.getElementById('js-input-uebersetzung-soll');
 var btn_geometrie = document.getElementById("js-btn-calc-geometrie");
+
+let label_z1 = document.getElementById('js-label-z1');
+let label_z2 = document.getElementById('js-label-z2');
+
+let show_abweichung_i = document.getElementById('js-show-abweichung-i');
+
+
+
+//Globale Variablen festlegen
+let btn_counter = 0;
+let m_n = 0;
+let z1 = 0;
+let z2 = 0;
+let i = "n.def";
+let i_soll = 0;
+let beta = 0;
+let alpha_n = 20;
+
+update_uebersetzung();
 
 //Wenn Button geklickt wird, dann wird Berechnung gestartet und das Ergebnis angezeigt
 btn_geometrie.addEventListener("click", function(event) {
 
     event.preventDefault;
+    //Werte auslesen    
 
-    //Werte auslesen
-    m_n = parseFloat(input_m_n.value);
-    z1 = parseFloat(input_z1.value);
-    z2 = parseFloat(input_z2.value);
-    beta = parseFloat(input_beta.value);
-    alpha_n = parseFloat(input_alpha.value);
     psi = 0.5;
 
     console.log("Modul: " + m_n);
     console.log("z1: " + z1);
     console.log("z2: " + z2);
+    console.log("i: " + i);
+    console.log("i_soll: " + i_soll);
     console.log("alpha: " + alpha_n);
     console.log("beta: " + beta);
+
+    if (z1 === 0 || z2 === 0) {
+        alert("Die Zähnezahlen dürfen nicht Null sein!");
+        return;
+    }
+
 
     //Zahnradpaarung definieren
     let paarung = new Zahnradpaarung(z1, z2, m_n, alpha_n, beta, psi);
@@ -51,33 +68,73 @@ btn_geometrie.addEventListener("click", function(event) {
 });
 
 input_m_n.addEventListener('change', function() {
+    m_n = parseFloat(input_m_n.value);
     update_button();
 });
 
 input_z1.addEventListener('change', function() {
+    z1 = parseFloat(input_z1.value);
+    update_label();
     update_button();
+    update_uebersetzung();
+    update_abweichung_i()
 });
 
 input_z2.addEventListener('change', function() {
+    z2 = parseFloat(input_z2.value);
+    update_label();
     update_button();
+    update_uebersetzung();
+    update_abweichung_i()
 });
 
 input_alpha.addEventListener('change', function() {
+    alpha_n = parseFloat(input_alpha.value);
     update_button();
+    update_uebersetzung();
 });
 
 input_beta.addEventListener('change', function() {
+    beta = parseFloat(input_beta.value);
     update_button();
 });
 
+input_i_soll.addEventListener('change', function() {
+    i_soll = parseFloat(input_i_soll.value);
+    update_abweichung_i();
+});
 
+function update_uebersetzung() {
+    i = -z2 / z1;
+    if (z1 == 0) {
+        i = "n.def";
+    }
+    input_i.value = i;
 
-
-
+    return i;
+}
 
 function update_button() {
     if (btn_counter > 0) {
         btn_geometrie.innerText = "Zahnradgeometrie erneut berechnen";
         btn_geometrie.className = "col-sm-12 btn btn-danger";
     }
+}
+
+function update_label() {
+    if (z1 > z2) {
+        label_z1.innerText = "Zähnezahl Zahnrad 1 (Rad)";
+        label_z2.innerText = "Zähnezahl Zahnrad 2 (Ritzel)";
+    } else if (z1 < z2) {
+        label_z1.innerText = "Zähnezahl Zahnrad 1 (Ritzel)";
+        label_z2.innerText = "Zähnezahl Zahnrad 2 (Rad)";
+    } else {
+        label_z1.innerText = "Zähnezahl Zahnrad 1";
+        label_z2.innerText = "Zähnezahl Zahnrad 2";
+    }
+}
+
+function update_abweichung_i() {
+    let abweichung = 100 * Math.abs((i_soll - i) / i_soll)
+    show_abweichung_i.innerText = "Abweichung : " + abweichung + "%";
 }
